c053ced40828fc1463b3261411c7eb332ee12bcd
Anibal Jose Rodriguez <arorive@protonmail.com>
2021-12-08 20:42:04 -0600

    add more option with yargs

--- 3. node-bases/app.js
+++ 3. node-bases/app.js
@@ -1,8 +1,26 @@
 const { createFile } = require('./helpers/multiply');
-const argv = require('yargs')..argv;
+const argv = require('yargs')
+                .option('b',{
+                    alias: 'base',
+                    type: 'number',
+                    demandOption:true
+                })
+                .option('l',{
+                    alias: 'listar',
+                    type: 'boolean',
+                    demandOption:true,
+                    default: false
+                })
+                .check((argv,option) => {
+                    if( isNaN(argv.base)){
+                        throw 'the base is not a number'
+                    }
+                    return true;
+                }).argv;
 
 //limpiar consola
 console.clear();
-
 console.log(argv);
-console.log(argv.base);
+createFile(argv.base, argv.l)
+.then( nombreArchivo => console.log(nombreArchivo))
+.catch( err => console.log(err) );
--- 3. node-bases/helpers/multiply.js
+++ 3. node-bases/helpers/multiply.js
@@ -1,19 +1,20 @@
 const fs = require('fs')
 
 //al tener el async ya el metodo defecto envia una promesa
-const createFile = async (base = 5) => {
+const createFile = async (base = 5, listar = false) => {
 
         try{
             let salida = '';
         
-            console.log("=========================");
-            console.log(`     Table ${base}       `);
-            console.log("=========================");
-        
             for(let i = 1; i <= 10; i++){
                 salida +=`${base} x ${i} = ${base*i} \n`;
             }
-            console.log(salida);
+            if(listar){
+                console.log("=========================");
+                console.log(`     Table ${base}       `);
+                console.log("=========================");
+                console.log(salida);
+            }
         
             //sino le especifico el path completo, creara el archivo en la misma carpeta del app.js
             fs.writeFileSync(`tabla-${base}.txt`, salida);
