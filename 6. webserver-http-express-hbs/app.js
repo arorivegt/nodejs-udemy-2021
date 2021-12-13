51b9977c726145a2c2c8070c0c6f02240c8bf453
Anibal Jose Rodriguez <arorive@protonmail.com>
2021-12-12 22:34:41 -0600

    Doing my webserver using only NodeJS

--- 6. webserver-http-express-hbs/app.js
+++ 6. webserver-http-express-hbs/app.js
@@ -3,7 +3,22 @@ const http = require('http');
 // req => the requirement of the user
 // res => the response of our server to give our user an answer
 http.createServer( ( req, res ) => {
-    res.write('Hola mundo');
+    console.log(req);
+
+    //res.writeHead(200,{ 'Content-Type': 'text/json'});
+    res.setHeader('Content-Disposition', 'attachment; filename=lis.csv');
+    res.writeHead(200,{ 'Content-Type': 'teaxt/csv'});
+    
+    /*const person = {
+        id: 1,
+        name : 'Anibal Rodriguez'
+    }
+    res.write(JSON.stringify(person));*/
+
+    res.write('id, name\n');
+    res.write('1, Anibal\n');
+    res.write('2, Jose\n');
+    res.write('3, Rodriguez\n');
     res.end();
 })
 .listen( 8080 );
